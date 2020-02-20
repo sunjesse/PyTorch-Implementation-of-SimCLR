33ec110d08ad4d0f36e6c30082a9cbc42b85b846
rexma <rexma1102@gmaiil.com>
2020-02-20 11:56:18 -0500

    optimzied

--- loss.py
+++ loss.py
@@ -6,11 +6,27 @@ import numpy as np
 class SimLoss(nn.Module):
     def __init__(self):
         super(SimLoss, self).__init__()
+    
+    #OPTIMIZED CODE, WE USE THIS ONE.
+    def forward(self, batch):
+        b = torch.mm(batch, batch.transpose(1,0))
+        norm = torch.norm(batch, p=2, dim=1).unsqueeze(1)
+        norm = torch.mm(norm, norm.transpose(1,0))
+        den = norm * (torch.ones(batch.shape[0]) - torch.eye(batch.shape[0])).cuda() * b
+        den = torch.sum(torch.exp(den), dim=1)
 
+        num = torch.zeros(batch.shape[0]).float().cuda()
+        for k in range(batch.shape[0]):
+            i, j = (k//2)*2, (k//2)*2+1
+            num[k] = b[i][j] / norm[i][j]
+        loss = torch.sum(-torch.log(torch.exp(num) / den))/batch.shape[0]
+        return loss
+    
+    #NAIVE UNOPTIMIZED CODE BELOW.
     def pairwise_sim(self, z_i, z_j):
         c = f.cosine_similarity(z_i, z_j, dim=0).float()
         return c
-    
+
     def soft_nll(self, i, j, batch):
         s_ij = torch.exp(self.pairwise_sim(batch[i-1], batch[j-1]))
         s_ik = torch.zeros(batch.shape[0]).cuda()
@@ -25,19 +41,5 @@ class SimLoss(nn.Module):
             loss[k] = self.soft_nll(2*k-1, 2*k, batch) + self.soft_nll(2*k, 2*k-1, batch)
         loss = torch.sum(loss) / loss.shape[0]
         return loss
-
-    def forward(self, batch):
-        b = torch.mm(batch, batch.transpose(1,0))
-        norm = torch.norm(batch, p=2, dim=1).unsqueeze(1)
-        norm = torch.mm(norm, norm.transpose(1,0))
-        den = norm * (torch.ones(batch.shape[0]) - torch.eye(batch.shape[0])).cuda() * b
-        den = torch.sum(torch.exp(den), dim=1)
-
-        num = torch.zeros(batch.shape[0]).float().cuda()
-        for k in range(batch.shape[0]):
-            i, j = (k//2)*2, (k//2)*2+1
-            num[k] = b[i][j] / norm[i][j]
-        loss = torch.sum(-torch.log(torch.exp(num) / den))/batch.shape[0]
-        return loss
-
+    
 
