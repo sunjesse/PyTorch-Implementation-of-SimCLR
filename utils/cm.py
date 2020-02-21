4dbd6526b02a51a92d50b57bc2fe69da6703967d
rexma <rexma1102@gmaiil.com>
2020-02-21 14:06:51 -0500

    made changes

--- utils/cm.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("ph.csv")
-OUTPATH = "/cluster/home/jessesun/diagnosis.txt"
-
-with open(OUTPATH, "a") as o:
-	for idx, row in df.iterrows():
-		o.write(str(row["Study ID"]) + " " +  str(row["cadiotoxicity_yes_no"]) + "\n")
